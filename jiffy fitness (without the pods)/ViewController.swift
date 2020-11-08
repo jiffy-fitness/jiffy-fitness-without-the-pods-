244aa578b5a3ee68aa84b6aac8a9a6e141fc41f4
fanflor theodore <fanflor.theodore@gmail.com>
2020-11-08 11:31:06 -0500

    Initial Commit

---
+++ jiffy fitness (without the pods)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jiffy fitness (without the pods)
+//
+//  Created by searto  on 11/8/20.
+//  Copyright © 2020 jiffy fitness. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
